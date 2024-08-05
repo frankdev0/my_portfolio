fe1857f5bddf5992de9f3d4b4ac7543ef778cf4b
unknown <nnaemekafrank400@gmail.com>
2024-08-05 11:14:24 +0100

    changes on commit navbar scroll

--- pages/components/hersosection.tsx
+++ pages/components/hersosection.tsx
@@ -10,10 +10,6 @@ export default function Herosection () {
         <div className={styles.herbg}>
         <div className={styles.heroPosition}>
         <div>
-        {/* <div >
-            <h1 className={styles.herotext}><span className={styles.intro}>Hi, I&lsquo;m</span> <span className={styles.name}>Frank</span></h1>
-            <h5 className={styles.introtext}>I develop web applications, user interfaces and 3D visuals</h5>
-        </div> */}
         <div className='mb-5'>
     <h1 className={styles.herotext}>
       <span className={styles.intro}>Hi, I&lsquo;m </span> 
